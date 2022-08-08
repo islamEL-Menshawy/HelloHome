871a2d8cc9ce0b72f5b01cc5be34851ca89b6b44
islamEL-Menshawy <islamelmenshwy@gmail.com>
2022-08-08 14:48:03 +0200

    add website to devoloper

--- app/Http/Controllers/API/CompoundController.php
+++ app/Http/Controllers/API/CompoundController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CompuendController extends Controller
+class CompoundController extends Controller
 {
     /**
      * Display a listing of the resource.
