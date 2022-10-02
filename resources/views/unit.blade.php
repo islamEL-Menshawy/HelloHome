3dbe53b020848df711959d42de8976181402ff05
emadsamy <emadsamy3030@gmail.com>
2022-10-02 17:36:44 +0200

    init

--- resources/views/unit.blade.php
+++ resources/views/unit.blade.php
@@ -358,7 +358,7 @@
                         <div class="unit-det-point">No parties or events</div>
                     </div>
 
-                    <div class="unit-details-points">
+                    <div class="unit-details-points udp-2">
                         <div class="unit-det-title white-color">Things to know</div>
                         <div class="unit-det-point">Check-in: 4:00 PM - 10:00 PM</div>
                         <div class="unit-det-point">Checkout: 10:00 AM</div>
